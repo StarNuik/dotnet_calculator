0633e2fbdcfe5e19b3f682ac913677b6b483c479
StarNuik <starnuik@gmail.com>
2024-09-22 20:13:06 +0300

    tokenizer passes tests

--- Calculator/Tokenizer.cs
+++ Calculator/Tokenizer.cs
@@ -2,6 +2,11 @@ namespace Calculator;
 
 public static class Tokenizer
 {
+	private static HashSet<string> SupportedFunc = new()
+	{
+		"pow", "sqrt",
+	};
+
 	public static Token[] Do(string @in)
 	{
 		var tokens = new List<Token>();
@@ -18,38 +23,75 @@ public static class Tokenizer
 
 	private static (Token token, string leftover) NextToken(string @in)
 	{
-		if (string.IsNullOrEmpty(@in)) {
+		if (string.IsNullOrEmpty(@in))
+		{
 			return (new Token{Key = TokenType.Eof}, "");
 		}
-		if (char.IsNumber(@in[0])) {
+		if (char.IsAsciiDigit(@in[0]))
+		{
 			return NextNumber(@in);
 		}
+		if (char.IsAsciiLetterLower(@in[0]))
+		{
+			return NextFunc(@in);
+		}
 
-		var tokenT = @in[0] switch {
+		var tokenT = @in[0] switch
+		{
 			'+' => TokenType.Add,
 			'-' => TokenType.Sub,
 			'*' => TokenType.Mul,
 			'/' => TokenType.Div,
 			'(' => TokenType.Open,
 			')' => TokenType.Close,
-			_ => throw new CantTokenizeException(@in[0].ToString()),
+			',' => TokenType.Comma,
+			_ => throw new CantTokenizeException(
+				$"symbol not supported: '{@in[0]}'"
+			),
 		};
 		
-		return (new Token{Key = tokenT}, @in.Substring(1));
+		return (new Token{Key = tokenT}, @in[1..]);
 	}
 
 	private static (Token token, string leftover) NextNumber(string @in)
 	{
+		var len = CountFirst(@in,
+			ch => char.IsAsciiDigit(ch) || ch == '.'
+		);
+
+		var token = new Token
+		{
+			Key = TokenType.Number,
+			Value = @in[..len],
+		};
+		return (token, @in[len..]);
+	}
+
+	private static (Token token, string leftover) NextFunc(string @in)
+	{
+		var len = CountFirst(@in, char.IsAsciiLetterLower);
+		var value = @in[..len];
+		if (!SupportedFunc.Contains(value))
+		{
+			throw new CantTokenizeException(
+				$"function not supported: '{value}'"
+			);
+		}
+		var token = new Token
+		{
+			Key = TokenType.Func,
+			Value = value,
+		};
+		return (token, @in[len..]);
+	}
+
+	private static int CountFirst(string @in, Func<char, bool> predicate)
+	{
 		var len = 0;
-		while (len < @in.Length
-			&& (char.IsNumber(@in[len]) || @in[len] == '.'))
+		while (len < @in.Length && predicate(@in[len]))
 		{
 			len++;
 		}
-		var token = new Token{
-			Key = TokenType.Number,
-			Value = @in.Substring(0, len),
-		};
-		return (token, @in.Remove(0, len));
+		return len;
 	}
 }
